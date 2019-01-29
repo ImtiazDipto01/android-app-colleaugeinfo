40ef22c3beff823ebd4c75039e59f451c5d7f2e2
imtiaz <programmer.imtiaz@gmail.com>
2019-01-29 09:15:51 +0600

    ADD- AddColleagueActivity and design added

---
+++ app/src/main/java/com/example/colleaugeinfo/AddColleagueActivity.java
@@ -0,0 +1,14 @@
+package com.example.colleaugeinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddColleagueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_colleague);
+    }
+}
