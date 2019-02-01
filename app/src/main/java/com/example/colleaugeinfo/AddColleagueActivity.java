919dc60d31da3680ccadffebb25e39d23db81878
imtiaz <programmer.imtiaz@gmail.com>
2019-02-01 09:18:01 +0600

    ADD- view initilize at AddColleagugeActivity

--- app/src/main/java/com/example/colleaugeinfo/AddColleagueActivity.java
+++ app/src/main/java/com/example/colleaugeinfo/AddColleagueActivity.java
@@ -1,14 +1,40 @@
 package com.example.colleaugeinfo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class AddColleagueActivity extends AppCompatActivity {
+
+    @BindView(R.id.etName)
+    EditText etName;
+    @BindView(R.id.etOffice)
+    EditText etOffice;
+    @BindView(R.id.etDesignation)
+    EditText etDesignation;
+    @BindView(R.id.etPhone)
+    EditText etPhone;
+    @BindView(R.id.etAddress)
+    EditText etAddress;
+    @BindView(R.id.btnSave)
+    Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_colleague);
+        ButterKnife.bind(this);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
